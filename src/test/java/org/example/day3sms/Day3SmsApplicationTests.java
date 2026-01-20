9c903f392d4e7fe07ebbb0b967332d1fc3021cb2
Rupendra Yadav <rupdendrayadav1022@gmail.com>
2026-01-20 11:59:15 +0530

    project initializer

---
+++ src/test/java/org/example/day3sms/Day3SmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.day3sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3SmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
